260de9c0efaab6c443818dd2b35e01734cce447f
JeffM <jeff@jeffreymclean.com>
2019-10-05 16:32:09 -0700

    Added Modal on Survery Form, started building out API Routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,9 +4,16 @@ var friendsData = require("../data/friends");
 module.exports = function(app) {
    
 
+
+    app.get("/api/friends", function(req, res){
+        res.json(friendsData); 
+    })
   
     app.post("/api/friends", function(req, res) {
-      res.json(friendsData);
-      console.log(friendsData)
+
+
+
+        
+      friendsData.push(req.body)
     });
   };
